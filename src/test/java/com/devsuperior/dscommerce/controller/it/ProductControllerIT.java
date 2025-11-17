90e793e825ae3f2c2796d1a09f6427d045c45312
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-11-16 22:25:15 -0300

    create ProductControllerIT

--- src/test/java/com/devsuperior/dscommerce/controller/it/ProductControllerIT.java
+++ src/test/java/com/devsuperior/dscommerce/controller/it/ProductControllerIT.java
@@ -1,11 +1,20 @@
 package com.devsuperior.dscommerce.controller.it;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class ProductControllersIT {
+public class ProductControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    void setUp() throws Exception {
+
+    }
 }
