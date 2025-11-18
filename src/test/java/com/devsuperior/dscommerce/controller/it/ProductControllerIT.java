387af45f9d88cc59f75f0baff61db270048395b5
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-11-18 09:14:21 -0300

    implements teste retornar todos os produtos paginados sem filtro por nome

--- src/test/java/com/devsuperior/dscommerce/controller/it/ProductControllerIT.java
+++ src/test/java/com/devsuperior/dscommerce/controller/it/ProductControllerIT.java
@@ -42,22 +42,20 @@ public class ProductControllerIT {
         resultActions.andExpect(jsonPath("$.content[0].name").value(productName));
         resultActions.andExpect(jsonPath("$.content[0].price").value(1250.0));
         resultActions.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
-
     }
 
-//    @Test
-//    public void findAllShouldReutrnAllProductsWhenNameParamIsEmpty() throws Exception {
-//
-//        ResultActions resultActions = mockMvc.perform(
-//                get("/products")
-//                        .accept(MediaType.APPLICATION_JSON)
-//        );
-//
-//        resultActions.andExpect(status().isOk());
-//        resultActions.andExpect(jsonPath("$.content[0].id").value(3L));
-//        resultActions.andExpect(jsonPath("$.content[0].name").value(productName));
-//        resultActions.andExpect(jsonPath("$.content[0].price").value(1250.0));
-//        resultActions.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
-//
-//    }
+    @Test
+    public void findAllShouldReutrnAllProductsWhenNameParamIsEmpty() throws Exception {
+
+        ResultActions resultActions = mockMvc.perform(
+                get("/products")
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$.content[0].id").value(1L));
+        resultActions.andExpect(jsonPath("$.content[0].name").value("The Lord of the Rings"));
+        resultActions.andExpect(jsonPath("$.content[0].price").value(90.5));
+        resultActions.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+    }
 }
